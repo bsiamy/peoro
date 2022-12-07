d7aabbe32ab65a73cc3e5a8613f0f8f60a83c34f
bsiamy <120028470+bsiamy@users.noreply.github.com>
2022-12-07 16:31:49 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.slick-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next')
+});
+$('.slick-slider-two').slick({
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
+$('.slick-slider-three').slick({
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 2500
+});
